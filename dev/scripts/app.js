7f281ebe814a211161a6dcf50b8a22dd6d6d2663
judapeng <164618761@qq.com>
2018-06-21 10:38:03 +0800

    第四次修改

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -71,8 +71,14 @@ const indexTpl = __webpack_require__(1)
 const headerTpl = __webpack_require__(2)
 const footerTpl = __webpack_require__(3)
 const homeTpl = __webpack_require__(4)
+const homeTpl2 = __webpack_require__(5)
+
 
 $('body').html(headerTpl+indexTpl+footerTpl)
+$('#container').html(homeTpl+homeTpl2)
+
+
+
 
 /***/ }),
 /* 1 */
@@ -84,19 +90,25 @@ module.exports = "<div id=\"container\"></div>"
 /* 2 */
 /***/ (function(module, exports) {
 
-module.exports = "<header id=\"header\">    <div class=\"headbox\">        <div class=\"logo\">            <img src=\"./imgss/logo.png\" alt=\"\">        </div>    </div></header>"
+module.exports = "<header id=\"header\">    <div class=\"headbox\">        <div class=\"logo\">            <img src=\"./imgess/logo.jpg\" alt=\"\">            <span>诚洁网</span>        </div>        <div class=\"mobil\">            <span></span>            <i>400-000-9898</i>        </div>        <div class=\"nav\">            <ul>                <li class=\"active\">首页</li>                <li>家庭服务</li>                <li>关于我们</li>                <li>                    <span class=\"login\">登录</span>                    /                    <span class=\"logon\">注册</span>                </li>            </ul>        </div>            </div></header>"
 
 /***/ }),
 /* 3 */
 /***/ (function(module, exports) {
 
-module.exports = "<footer></footer>"
+module.exports = "<footer> <div class=\"foots\">    <div class=\"leftcopy\">        &copy;诚洁网 京ICP备13032113<span></span>京公网安备11010802013311    </div>    <div class=\"rightnav\">        <div class=\"jiating\">            <a href=\"\">家庭服务</a>        </div>        <div class=\"richang\">            <a href=\"\">日常保洁</a>        </div>        <div class=\"guanyu\">            <a href=\"\">关于我们</a>        </div>    </div> </div></footer>"
 
 /***/ }),
 /* 4 */
 /***/ (function(module, exports) {
 
-module.exports = ""
+module.exports = "<div id=\"banner\"></div><div id=\"job-nav\">    <div class=\"joblogo\">        <h3>轻触屏幕 家务事无忧</h3>        <span>全方位的家庭服务，一键下单，尽享清闲生活</span>    </div>    <div class=\"cleanbox\">        <div class=\"blueball\">            <div>保洁服务</div>        </div>        <div class=\"clean-list\">            <div class=\"yuan1\"></div>            <div class=\"shuxian1\">                <div class=\"hengxian1\">                    <p>日 常 保 洁</p>                        <span></span>                      </div>                <div class=\"hengxian2\">                    <p>深 度 保 洁</p>                        <span></span>                 </div>                <div class=\"hengxian3\">                    <p>长 期 保 洁</p>                        <span></span>                 </div>                <div class=\"hengxian4\">                    <p>擦 玻 璃</p>                        <span></span>                 </div>                <div class=\"hengxian5\">                    <p>新 居 开 荒</p>                        <span></span>                 </div>                <div class=\"hengxian6\">                    <p>厨 房 保 养</p>                        <span></span>                 </div>            </div>            <div class=\"yuan2\"></div>            <div class=\"yuan3\"></div>            <div class=\"shuxian2\">                <div class=\"leftxian1\">                    <p>卫生间保养</p>                        <span></span>                 </div>                <div class=\"leftxian2\">                    <p>地板打蜡</p>                        <span></span>                 </div>                <div class=\"leftxian3\">                    <p>厨皮沙发保养</p>                        <span></span>                 </div>            </div>            <div class=\"yuan4\"></div>        </div>    </div></div>"
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports) {
+
+module.exports = "<div id=\"spe\">    <div class=\"spebox\">        <div class=\"joblogo\">            <h3>轻触屏幕 家务事无忧</h3>            <span>全方位的家庭服务，一键下单，尽享清闲生活</span>        </div>    <div class=\"yuantu\">        <div class=\"zhuanye\">            <img src=\"../imgess/zhuanye.png\" alt=\"\">            <h4>专业</h4>            <p>服务人员严格筛选、择优录取；</p>            <i>系统培训、专业工具、标准化流程</i>        </div>        <div class=\"bianjie\">            <img src=\"../imgess/bianjie.png\" alt=\"\">            <h4>便捷</h4>            <p>随时随地一键下单；个性化需求智能匹配；</p>            <i>无需现金、轻松支付</i>        </div>        <div class=\"anquan\">             <img src=\"../imgess/anquan.png\" alt=\"\">            <h4>安全</h4>            <p>职务人员100%实名认证</p>            <i>上门服务家政保驾护航</i>        </div>    </div></div></div>"
 
 /***/ })
 /******/ ]);
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,5 +2,10 @@ const indexTpl = require('./views/index.html')
 const headerTpl = require('./views/header.html')
 const footerTpl = require('./views/footer.html')
 const homeTpl = require('./views/home.html')
+const homeTpl2 = require('./views/home2.html')
+
 
 $('body').html(headerTpl+indexTpl+footerTpl)
+$('#container').html(homeTpl+homeTpl2)
+
+
